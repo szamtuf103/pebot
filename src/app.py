5a5a2ce0b57d470b92c41e9edb96cd49b8e16455
szamtuf103 <konradhorczak@icloud.com>
2024-01-18 13:50:39 +0100

    initial ez

---
+++ src/app.py
@@ -0,0 +1,16 @@
+#/bin/bash/python3
+
+from geometry_msgs import PoseWithCovariance
+
+class App:
+
+    def __init__(self, ez):
+        self.ez = ez        
+
+
+def main():
+    app = App()
+
+
+if __name__ == "__main__":
+    main()
